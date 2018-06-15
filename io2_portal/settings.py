e36be06e35b454b7f3e8976b49743507518b96f3
Landung Setiawan <landungs@uw.edu>
2018-06-15 16:41:08 -0700

    Add ALLOWED HOSTS

--- io2_portal/settings.py
+++ io2_portal/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0f7$d08jz3t1_4-9tx*4rr)-&)6zz$jfpol+fwy4=yze_x-sv='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['io2api-dev.zh2dmitwm7.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
